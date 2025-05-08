a7c59e0673fb75a20ac9cc1cb3f4fc95ee8c7287
sundar <Sbashyal@myseneca.ca>
2025-05-08 13:12:35 -0400

    Completed lab1d.py for Lab 1

---
+++ lab1d.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''Description: Do math using variables and print result'''
+
+
+
+x = 10
+
+y = 2
+
+z = 5
+
+
+
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(x + y * z))
+
+
