36538e2e13f12bb4cf1add8d219496221dae6959
sundar bashyal <sbashyal@myseneca.ca>
2025-05-22 13:06:25 -0400

    Added lab1d

--- lab1d.py
+++ lab1d.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-'''Description: This program demonstrates basic math operations using variables.'''
 
-# Define the variables
 x = 10
 y = 2
 z = 5
 
-# Perform the mathematical operation and print the result
-result = x + y * z  # According to PEMDAS, multiplication happens before addition
+result = x + y * z
+
 print(f"{x} + {y} * {z} = {result}")
