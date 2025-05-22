1e1c4777e691bf40be02598d120061d5c27727d6
sundar bashyal <sbashyal@myseneca.ca>
2025-05-22 12:40:37 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program will ask how old Isaac is.'''
+
+# Assigning 'Isaac' to the variable 'name'
+name = 'Isaac'
+
+# Printing the desired output
+print('How old are you ' + name + '?')
