26dd90660a7bd35878f08df8a133445190489e3e
sundar bashyal <sbashyal@myseneca.ca>
2025-05-22 13:04:39 -0400

    Added lab1c

--- lab1c.py
+++ lab1c.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
-'''Description: This program will demonstrate integer operations and string concatenation.'''
 
-# Assigning values to integer objects
-num1 = 5
-num2 = 10
-
-# Performing math operation (addition)
-sum = num1 + num2
-
-# Remove the print statements for num1, num2, and sum
-# They should not be printed according to the expected output
-
-# Create the name and age variables
-name = 'Isaac'
+name = "Isaac"
 age = 72
 
-# Correctly print the desired output in the required format
-print(name + ' is ' + str(age) + ' years old!')
-
+print(f"{name} is {age} years old!")
